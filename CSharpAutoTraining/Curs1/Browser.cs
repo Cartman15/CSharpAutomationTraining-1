d0956445b8b9e3c6ef7ef26276ef932fa2552973
Carmen Vele <Carmen.Vele@endava.com>
2017-11-03 14:30:13 +0200

    homework1

---
+++ CSharpAutoTraining/Curs1/Browser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAutoTraining.Curs1
+{
+   public class Browser
+    {
+        public int WindowWidth = 1024;
+        public int WindowHeight = 768;
+        public void ChangeWindowWidth(int increment)
+        {
+            WindowWidth = WindowWidth + increment;
+            Console.WriteLine("WindowWidth is: {0}", WindowWidth);
+        }
+        public void ChangeWindowHeight(int increment)
+        {
+            WindowHeight = WindowHeight + increment;
+            Console.WriteLine("WindowHeight is: {0}", WindowHeight);
+        }
+
+        public void DisplayBrowserResolution()
+        {
+            Console.WriteLine("Browser resolution is: {0}", WindowWidth + "x" + WindowHeight);
+        }
+    }
+}
---
+++ CSharpAutoTraining/Curs1/Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAutoTraining.Curs1
+{
+    class Test
+    {
+        static void Main()
+        {
+            Console.WriteLine("Program that displays browser resolution:");
+            Browser browser = new Browser();                      
+            browser.ChangeWindowWidth(896);            
+            browser.ChangeWindowHeight(312);
+            browser.DisplayBrowserResolution();
+            Console.ReadKey();
+        }
+    }
+}
