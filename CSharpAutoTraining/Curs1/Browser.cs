75bc12e328955c581657e725b49ec647a6cf5714
Ioana Antal <Ioana.Antal@endava.com>
2017-11-09 15:52:37 +0200

    Practice 1

---
+++ CSharpAutoTraining/Curs1/Browser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAutoTraining.Curs1
+{
+    public class Browser
+    {
+        //variable
+        public int WindowWidth = 0;
+        public int WindowHeight = 0;
+
+        //method
+        public void ChangeWidnowWidth(int increment)
+        {
+            WindowWidth = WindowWidth + increment;
+        }
+
+        //method
+        public void ChangeWindowHeight(int increment)
+        {
+            WindowHeight = WindowHeight + increment;
+        }
+
+        public void DisplayBrowserResolution()
+        {
+            Console.WriteLine(WindowWidth + " " + WindowHeight);
+        }
+    }
+}
---
+++ CSharpAutoTraining/Curs1/Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAutoTraining.Curs1
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Browser browser = new Browser();
+
+            browser.ChangeWidnowWidth(1080);
+            browser.ChangeWindowHeight(1920);
+
+            browser.DisplayBrowserResolution();
+        }
+    }
+}
